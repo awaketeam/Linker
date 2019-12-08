1f98b4612ec12a9fbb1a9a3aed2b5a40193b55bd
kx <515403893@qq.com>
2019-12-09 01:50:30 +0800

    iOS 集成 cocopods，替换App icon，名称。

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              '老谢 have pushed the button this many times:',
             ),
             Text(
               '$_counter',
